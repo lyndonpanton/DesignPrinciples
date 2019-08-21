b56ad85d34bafa43236fdf505f473f5cd2169fd8
Lyndon Panton <LyndonPanton@gmail.com>
2019-08-21 18:57:29 +0100

    added click event listener to navigation buttton

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 window.onload = function(event) {
-	
+    var navButton = document.getElementById("nav-button");
+    navButton.addEventListener("click", toggleMenu);
+
+    function toggleMenu(event) {
+
+    }
 };
