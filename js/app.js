04b7e85702de094453f508960cb59a4e5ac833e7
Lyndon Panton <LyndonPanton@gmail.com>
2019-08-22 18:01:32 +0100

    added dynamic year to footer

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ window.onload = function(event) {
     var navButton = document.getElementById("nav-button");
     navButton.addEventListener("click", toggleMenu);
 
+    var copyrightYear = document.getElementById("copyright-year");
+    copyrightYear.textContent = (new Date()).getFullYear();
+
     function toggleMenu(event) {
         var navigation = document.getElementById("nav-list");
         navigation.classList.toggle("nav-open");
